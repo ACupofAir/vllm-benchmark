e3162c75990acec298ac0bf7379fcb5a3b7c0ead
gc-fu <guancheng.fu@intel.com>
2024-06-21 15:31:02 +0800

    fix

--- single_card/chatglm3-fp8.sh
+++ single_card/chatglm3-fp8.sh
@@ -20,4 +20,4 @@ export MAX_NUM_SEQS=180
 export TENSOR_PARALLEL_SIZE=1
 export GPU_UTILIZATION_RATE=0.95
 
-python ./benchmark_vllm_throughput.py --backend vllm --model $MODEL --num-prompts $NUM_PROMPTS --input-len $IN_LEN --output-len $OUT_LEN --trust-remote-code --enforce-eager --dtype float16 --device xpu --load-in-low-bit $LOW_BIT --gpu-memory-utilization $GPU_UTILIZATION_RATE --max-model-len $MAX_MODEL_LEN --max-num-batched-tokens $MAX_NUM_BATHCED_TOKENS --max-num-seqs $MAX_NUM_SEQS --tensor-parallel-size 1
+python /llm/benchmark_vllm_throughput.py --backend vllm --model $MODEL --num-prompts $NUM_PROMPTS --input-len $IN_LEN --output-len $OUT_LEN --trust-remote-code --enforce-eager --dtype float16 --device xpu --load-in-low-bit $LOW_BIT --gpu-memory-utilization $GPU_UTILIZATION_RATE --max-model-len $MAX_MODEL_LEN --max-num-batched-tokens $MAX_NUM_BATHCED_TOKENS --max-num-seqs $MAX_NUM_SEQS --tensor-parallel-size 1
