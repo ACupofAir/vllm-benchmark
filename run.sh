226d8b703b3aa1654a0679f92ddfbe17d259cdb7
gc-fu <guancheng.fu@intel.com>
2024-06-24 14:04:06 +0800

    fix

--- run.sh
+++ run.sh
@@ -2,7 +2,6 @@
 
 # TEST_DIRS=("single_card" "dual_cards")
 # TEST_DIRS=("single_card")
-TEST_DIRS=("dual_cards")
 LOG_DIR="logs"
 
 if [ ! -d "$LOG_DIR" ]; then
@@ -16,15 +15,21 @@ for TEST_DIR in "${TEST_DIRS[@]}"; do
     fi
 
     for SCRIPT in "$TEST_DIR"/*.sh; do
+        if [ ! -f "$SCRIPT" ]; then
+            continue
+        fi
+
         SCRIPT_NAME=$(basename "$SCRIPT")
-        LOG_FILE="${LOG_DIR}/${TEST_DIR}_${SCRIPT_NAME}.log"
+        PARENT_DIR=$(basename "$(dirname "$TEST_DIR")")
+        CURRENT_DIR=$(basename "$TEST_DIR")
+        LOG_FILE="${LOG_DIR}/${PARENT_DIR}_${CURRENT_DIR}_${SCRIPT_NAME}.log"
         echo "Running $SCRIPT, logging to $LOG_FILE..."
 
         echo "### Script Content: ${SCRIPT}" > "$LOG_FILE"
         cat "$SCRIPT" >> "$LOG_FILE"
         echo -e "\n### Script Output:\n" >> "$LOG_FILE"
 
-        bash ./"$SCRIPT" &>> "$LOG_FILE"
+        bash "$SCRIPT" &>> "$LOG_FILE"
         echo "$SCRIPT finished. Output logged to $LOG_FILE"
         echo
     done
