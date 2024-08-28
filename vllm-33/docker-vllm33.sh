be38f0cad8056bf7b224c17cf27d8e9cb70db232
WangJun <thoughts.times@gmail.com>
2024-08-28 10:22:37 +0800

    update vllm-33 script

---
+++ vllm-33/docker-vllm33.sh
@@ -0,0 +1,14 @@
+export DOCKER_IMAGE=intelanalytics/ipex-llm-serving-xpu:latest
+export CONTAINER_NAME=junwang-vllm33
+
+docker rm -f $CONTAINER_NAME
+sudo docker run -itd \
+        --privileged \
+        --net=host \
+        --device=/dev/dri \
+        -v /home/intel/LLM/:/llm/models \
+        -v /home/intel/junwang/:/workspace \
+        -e no_proxy=localhost,127.0.0.1 \
+        --name=$CONTAINER_NAME \
+        --shm-size="16g" \
+        $DOCKER_IMAGE
--- vllm-54/README.md
+++ vllm-54/README.md
@@ -7,12 +7,16 @@ python vllm-out-verify.py <model-path> <tp-num>
 ```
 
 ## online serve benchmark
+
 1. start serve
 change the <model-path>, config <model-id> and tp/pp setting in `vllm-serve.sh`, then:
+
 ```bash
 bash vllm-serve.sh
 ```
+
 2. benchmark(default 1k-512)
+
 ```bash
 cd /llm
 python vllm_online_benchmark.py <model-id> <max-seq>
