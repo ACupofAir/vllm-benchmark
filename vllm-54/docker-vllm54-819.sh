8d5fa7e858631322be51518659bebfa1c8c219df
WangJun <thoughts.times@gmail.com>
2024-08-30 13:21:48 +0800

    fix vllm image version

--- vllm-54/docker-vllm54-819.sh
+++ vllm-54/docker-vllm54-819.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-export DOCKER_IMAGE=10.239.45.10/arda/intelanalytics/vllm-ipex-054:20240812
+export DOCKER_IMAGE=10.239.45.10/arda/intelanalytics/vllm-ipex-054:20240819
 export CONTAINER_NAME=junwang-vllm54-bmt
 
 docker rm -f $CONTAINER_NAME
 sudo docker run -itd \
-        --net=host \
-        --device=/dev/dri \
-        --name=$CONTAINER_NAME \
-        -v /home/intel/LLM:/llm/models/ \
-        -v /home/intel/junwang:/workspace \
-        -e no_proxy=localhost,127.0.0.1 \
-        --shm-size="16g" \
-        $DOCKER_IMAGE
+	--net=host \
+	--device=/dev/dri \
+	--name=$CONTAINER_NAME \
+	-v /home/intel/LLM:/llm/models/ \
+	-v /home/intel/junwang:/workspace \
+	-e no_proxy=localhost,127.0.0.1 \
+	--shm-size="16g" \
+	$DOCKER_IMAGE
+
