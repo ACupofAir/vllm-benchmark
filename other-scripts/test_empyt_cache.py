670641215f191347c2168b84e8194b8511803f7a
ACupofAir <thoughts.times@gmail.com>
2024-09-09 09:53:15 +0800

    nothing

--- other-scripts/test_empyt_cache.py
+++ other-scripts/test_empyt_cache.py
@@ -6,7 +6,8 @@ a1 = torch.zeros((600, 1000, 1000), device="xpu:0")
 torch.xpu.empty_cache()
 a1 = torch.zeros((600, 1000, 1000), device="xpu:0") + 2
 torch.xpu.empty_cache()
-print(torch.xpu.memory_allocated())
+print(f"reserved {torch.xpu.memory.memory_reserved()/(1024**2)}MB")
+print(f"allocated {torch.xpu.memory.memory_allocated()/(1024**2)}MB")
 
 a2 = torch.zeros((600, 1000, 1000), device="xpu:0")
 a3 = torch.zeros((600, 1000, 1000), device="xpu:0")
@@ -14,4 +15,5 @@ a4 = torch.zeros((600, 1000, 1000), device="xpu:0")
 a5 = torch.zeros((600, 1000, 1000), device="xpu:0")
 
 torch.xpu.empty_cache()
-print(torch.xpu.memory_allocated())
+print(f"reserved {torch.xpu.memory.memory_reserved()/(1024**2)}MB")
+print(f"allocated {torch.xpu.memory.memory_allocated()/(1024**2)}MB")
