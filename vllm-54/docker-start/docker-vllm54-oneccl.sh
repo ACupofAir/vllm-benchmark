531a7948822fa927c3b3c58524396d3345a8530f
ACupofAir <thoughts.time@gmail.com>
2024-10-20 21:36:44 +0800

    [backup] 2024Oct.20

--- vllm-54/docker-start/docker-vllm54-oneccl.sh
+++ vllm-54/docker-start/docker-vllm54-oneccl.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-export DOCKER_IMAGE=intelanalytics/ipex-llm-serving-xpu:latest
-export CONTAINER_NAME=junwang-serving-xpu-container-lastest
+export DOCKER_IMAGE=intelanalytics/ipex-llm-serving-xpu:debug_oneccl_0.0.4.rc
+export CONTAINER_NAME=junwang-004rc
 
 docker rm -f $CONTAINER_NAME
 sudo docker run -itd \
-    --privileged \
 	--net=host \
 	--device=/dev/dri \
 	--name=$CONTAINER_NAME \
 	-v /home/intel/LLM:/llm/models/ \
-	-v /home/intel/junwang:/workspace \
+    -v /home/intel/junwang:/workspace \
 	-e no_proxy=localhost,127.0.0.1 \
 	--shm-size="16g" \
 	$DOCKER_IMAGE
---
+++ vllm-54/tp-pp-qwen14b.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+model="/llm/models/Qwen1.5-14B-Chat"
+served_model_name="Qwen1.5-14B-Chat"
+
+export CCL_WORKER_COUNT=4
+# export FI_PROVIDER=shm
+# export CCL_ATL_TRANSPORT=ofi
+# export CCL_ZE_IPC_EXCHANGE=sockets
+# export CCL_ATL_SHM=1
+ 
+export USE_XETLA=OFF
+export SYCL_PI_LEVEL_ZERO_USE_IMMEDIATE_COMMANDLISTS=2
+export TORCH_LLM_ALLREDUCE=0
+ 
+source /opt/intel/1ccl-wks/setvars.sh
+
+python -m ipex_llm.vllm.xpu.entrypoints.openai.api_server \
+  --served-model-name $served_model_name \
+  --port 8001 \
+  --model $model \
+  --trust-remote-code \
+  --gpu-memory-utilization 0.9 \
+  --device xpu \
+  --dtype float16 \
+  --enforce-eager \
+  --load-in-low-bit fp8 \
+  --max-model-len 3000 \
+  --max-num-batched-tokens 3000 \
+  --max-num-seqs 12 \
+  -tp 2 \
+  -pp 2 
