243805fb5c02399c191a7e8fb15b2a9190e50931
WangJun <thoughts.times@gmail.com>
2024-10-24 09:32:46 +0800

    nothing

--- openwebui/openwebui.sh
+++ openwebui/openwebui.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 export DOCKER_IMAGE=ghcr.io/open-webui/open-webui:main
 export CONTAINER_NAME=junwang-open-webui
@@ -6,6 +5,7 @@ export CONTAINER_NAME=junwang-open-webui
 docker rm -f $CONTAINER_NAME
 
 docker run -itd \
+           --net=host \
            -p 3000:8080 \
            -e OPENAI_API_KEY=NULL \
            -e OPENAI_API_BASE_URL=http://localhost:8001/v1 \
