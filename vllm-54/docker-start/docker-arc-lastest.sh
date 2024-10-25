cf69df7b86b8e9f181a5248c7a0196791cac42b9
Ariadne <wyn2000330@126.com>
2024-10-25 08:50:01 +0800

    [update] sep arc and wsarc

---
+++ vllm-54/docker-start/docker-arc-lastest.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+export DOCKER_IMAGE=intelanalytics/ipex-llm-serving-xpu:latest
+export CONTAINER_NAME=junwang-llm-lastest
+
+docker rm -f $CONTAINER_NAME
+sudo docker run -itd \
+	--privileged \
+	--net=host \
+	--device=/dev/dri \
+	--name=$CONTAINER_NAME \
+	-v /home/disk1/models:/llm/models/ \
+	-v /home/arda/junwang:/llm/workspace \
+	-e no_proxy=localhost,127.0.0.1 \
+	-e http_proxy=http://proxy.iil.intel.com:911 \
+	-e https_proxy=http://proxy.iil.intel.com:911 \
+	--shm-size="16g" \
+	$DOCKER_IMAGE
