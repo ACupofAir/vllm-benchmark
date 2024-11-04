46e4ee0ae3180e54cf9cee767e0f00cdabf54e68
Ariadne <wyn2000330@126.com>
2024-11-04 12:15:38 +0800

    nothing

---
+++ docker-start/docker-arc-062.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+export DOCKER_IMAGE=intelanalytics/vllm-ipex-experimental:txytest_1010
+export CONTAINER_NAME=junwang-llm-062
+
+docker rm -f $CONTAINER_NAME
+sudo docker run -itd \
+	--privileged \
+	--net=host \
+	--device=/dev/dri \
+	--name=$CONTAINER_NAME \
+	-v /mnt/disk1/models:/llm/models/ \
+	-v /home/arda/junwang:/llm/workspace \
+	-e no_proxy=localhost,127.0.0.1 \
+	-e http_proxy=http://proxy.iil.intel.com:911 \
+	-e https_proxy=http://proxy.iil.intel.com:911 \
+	--shm-size="16g" \
+	$DOCKER_IMAGE
