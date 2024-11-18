fd4ea04b05662fd9783f46cb35692bcb6ffd5525
WangJun <thoughts.times@gmail.com>
2024-11-18 10:28:46 +0800

    [REMOVE] rm useless blank

--- openwebui/vllm-backend-llama3.1-8b-openaikey.sh
+++ openwebui/vllm-backend-llama3.1-8b-openaikey.sh
@@ -8,11 +8,11 @@ export FI_PROVIDER=shm
 export CCL_ATL_TRANSPORT=ofi
 export CCL_ZE_IPC_EXCHANGE=sockets
 export CCL_ATL_SHM=1
- 
+
 export USE_XETLA=OFF
 export SYCL_PI_LEVEL_ZERO_USE_IMMEDIATE_COMMANDLISTS=2
 export TORCH_LLM_ALLREDUCE=0
- 
+
 source /opt/intel/1ccl-wks/setvars.sh
 
 python -m ipex_llm.vllm.xpu.entrypoints.openai.api_server \
