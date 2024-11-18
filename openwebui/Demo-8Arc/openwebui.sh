8b251154aae6d48784380944d91e47ff83e13c32
WangJun <thoughts.times@gmail.com>
2024-11-18 16:19:16 +0800

    [REFACTOR] change dir struct

---
+++ openwebui/Demo-8Arc/openwebui.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+export DOCKER_IMAGE=ghcr.io/open-webui/open-webui:main
+export CONTAINER_NAME=junwang-open-webui
+
+docker rm -f $CONTAINER_NAME
+
+docker run -itd \
+           -p 3000:8080 \
+           -e OPENAI_API_KEY=intel123 \
+           -e OPENAI_API_BASE_URL=http://10.240.203.84:8001/v1 \
+           -v open-webui:/app/backend/data \
+           --name $CONTAINER_NAME \
+           --restart always $DOCKER_IMAGE
---
+++ openwebui/Demo-8Arc/readme.md
@@ -0,0 +1,13 @@
+# IPEX vLLM Serving with openwebui
+
+## Backend with vLLM Serving
+
+### Start docker
+
+### Start IPEX vLLM Serving
+
+## Frontedn with openwebui
+
+1. start docker
+2. patch openwebui for context-length limit
+2. verify
