db82af75041a1ae59d0de297d870e7fce7431d5b
ACupofAir <thoughts.time@gmail.com>
2024-11-18 10:07:45 +0000

    84 demo

---
+++ openwebui/vllm-backend-qwen2.5-72b-openaikey.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+model="/llm/models/Qwen2.5-72B-Instruct"
+served_model_name="Qwen2.5-72B-Instruct"
+
+# export SYCL_CACHE_PERSISTENT=1
+export CCL_WORKER_COUNT=4
+export FI_PROVIDER=shm
+export CCL_ATL_TRANSPORT=ofi
+export CCL_ZE_IPC_EXCHANGE=sockets
+export CCL_ATL_SHM=1
+ 
+export USE_XETLA=OFF
+export SYCL_PI_LEVEL_ZERO_USE_IMMEDIATE_COMMANDLISTS=2
+export TORCH_LLM_ALLREDUCE=0
+ 
+source /opt/intel/1ccl-wks/setvars.sh
+
+python -m ipex_llm.vllm.xpu.entrypoints.openai.api_server \
+  --served-model-name $served_model_name \
+  --port 8001 \
+  --model $model \
+  --trust-remote-code \
+  --gpu-memory-utilization 0.9 \
+  --device xpu \
+  --dtype float16 \
+  --enforce-eager \
+  --load-in-low-bit fp8 \
+  --max-model-len 4000 \
+  --max-num-batched-tokens 4000 \
+  --max-num-seqs 12 \
+  --api-key intel123 \
+  --tensor-parallel-size 8 \
+  --distributed-executor-backend ray
