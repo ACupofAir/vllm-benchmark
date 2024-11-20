99c044fdb0486f310e30f2f7825eb84e119da242
intel <intel@WS-POC-3.bj.intel.com>
2024-11-20 09:57:22 +0800

    [ADD] demo-4arc

---
+++ openwebui/Demo-4Arc/backend-ipex-docker.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+export DOCKER_IMAGE=intelanalytics/ipex-llm-serving-xpu:2.2.0-b6
+export CONTAINER_NAME=ipex-llm-b6
+
+docker rm -f $CONTAINER_NAME
+sudo docker run -itd \
+	--privileged \
+	--net=host \
+	--device=/dev/dri \
+	--name=$CONTAINER_NAME \
+	-v /data:/llm/models/ \
+	-v /home/intel/Demo-4Arc:/llm/workspace \
+	-e no_proxy=localhost,127.0.0.1 \
+	-e http_proxy=http://proxy.iil.intel.com:911 \
+	-e https_proxy=http://proxy.iil.intel.com:911 \
+	--shm-size="32g" \
+	$DOCKER_IMAGE
---
+++ openwebui/Demo-4Arc/frontend-openwebui-docker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+export DOCKER_IMAGE=ghcr.io/open-webui/open-webui:main
+export CONTAINER_NAME=demo-open-webui
+
+docker rm -f $CONTAINER_NAME
+
+docker run -itd \
+           -p 3000:8080 \
+           -e OPENAI_API_KEY=intel123 \
+           -e OPENAI_API_BASE_URL=http://10.238.154.143:8001/v1 \
+           -v open-webui:/app/backend/data \
+           --name $CONTAINER_NAME \
+           --restart always $DOCKER_IMAGE
