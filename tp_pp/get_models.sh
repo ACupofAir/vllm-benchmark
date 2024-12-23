da5b55db8dc5be5b2293277561d2159652b7306b
ACupofAir <thoughts.times@gmail.com>
2024-12-23 16:54:32 +0800

    add llmperf

--- tp_pp/get_models.sh
+++ tp_pp/get_models.sh
@@ -1,1 +1,1 @@
-curl http://localhost:8001/v1/models
+curl http://localhost:8000/v1/models -H "Content-Type: application/json" -H "Authorization: Bearer 123456"
---
+++ tp_pp/tp1pp1-qwen25-7b.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+model="/llm/models/Qwen2.5-7B-Instruct"
+served_model_name="Qwen2.5-7B-Instruct"
+
+export CCL_WORKER_COUNT=2
+export FI_PROVIDER=shm
+export CCL_ATL_TRANSPORT=ofi
+export CCL_ZE_IPC_EXCHANGE=sockets
+export CCL_ATL_SHM=1
+ 
+export USE_XETLA=OFF
+export SYCL_PI_LEVEL_ZERO_USE_IMMEDIATE_COMMANDLISTS=2
+export TORCH_LLM_ALLREDUCE=0
+ 
+source /opt/intel/1ccl-wks/setvars.sh
+
+python -m ipex_llm.vllm.xpu.entrypoints.openai.api_server \
+  --served-model-name $served_model_name \
+  --port 8000 \
+  --model $model \
+  --trust-remote-code \
+  --block-size 8 \
+  --gpu-memory-utilization 0.95 \
+  --device xpu \
+  --dtype float16 \
+  --enforce-eager \
+  --load-in-low-bit fp8 \
+  --max-model-len 2000 \
+  --max-num-batched-tokens 3000 \
+  --tensor-parallel-size 1 \
+  --disable-async-output-proc \
+  --api-key 123456
+  --distributed-executor-backend ray
