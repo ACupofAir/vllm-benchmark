9338f823c3261cae075709e9aaa0f0d20d5a9671
ACupofAir <thoughts.times@gmail.com>
2025-01-22 09:54:01 +0800

    update openwebui

--- openwebui/Demo-4Arc/frontend-openwebui-docker.sh
+++ openwebui/Demo-4Arc/frontend-openwebui-docker.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 export DOCKER_IMAGE=ghcr.io/open-webui/open-webui:main
-export CONTAINER_NAME=demo-open-webui
+export CONTAINER_NAME=junwang-open-webui
 
 docker rm -f $CONTAINER_NAME
 
 docker run -itd \
            -p 3000:8080 \
+           -e OPENAI_API_BASE_URL=http://10.112.228.104:8001/v1 \
            -e OPENAI_API_KEY=intel123 \
-           -e OPENAI_API_BASE_URL=http://10.238.154.143:8001/v1 \
            -v open-webui:/app/backend/data \
            --name $CONTAINER_NAME \
            --restart always $DOCKER_IMAGE
