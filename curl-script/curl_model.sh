fe39b5abbdb9d8111bcfa6e8b8ae15fb2b7fc10f
WangJun <thoughts.times@gmail.com>
2025-03-04 09:07:33 +0800

    update curl scripts

--- curl-script/curl_model.sh
+++ curl-script/curl_model.sh
@@ -1,6 +1,6 @@
 set -x
 model_name=$1
-port=$2
+port=${2:-"8001"}
 curl http://localhost:$port/v1/completions \
   -H "Content-Type: application/json" \
   -H "Authorization: Bearer intel123" \
--- curl-script/get_models.sh
+++ curl-script/get_models.sh
@@ -1,3 +1,3 @@
-set +x
-port=$1
+set -x
+port=${1:-"8001"}
 curl http://localhost:$port/v1/models -H "Content-Type: application/json" -H "Authorization: Bearer intel123"
